6c2ece77fc7832fd965e4ae92301472c39449813
James Webb <jwebb@intelligenttransactions.com>
2019-09-28 15:16:47 -0400

    Initial Code Commit

---
+++ SP-Y4C/SP-Y4C/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SP_Y4C.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
