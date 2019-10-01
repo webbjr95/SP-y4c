b9314cf2264999f748caffaa39ae1c836db39eed
Devany Noblit <47183931+DNoblit@users.noreply.github.com>
2019-09-30 23:37:01 -0400

    Added QuestionsTable Controller, DbContext, Data, and put the link to SSMS in the .json file
    Added QuestionsTable Controller, DbContext, Data, and put the link to SSMS in the .json file

---
+++ SP-Y4C/SP-Y4C/Models/QuestionsTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SP_Y4C.Models
+{
+    public class QuestionsTable : Controller
+{
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}
