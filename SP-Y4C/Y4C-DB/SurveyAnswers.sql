36c192f49a176a32fcc8836478b26e821f0a5f61
James Webb <jwebb@intelligenttransactions.com>
2020-01-28 16:05:54 -0500

    Adding in common files

---
+++ SP-Y4C/Y4C-DB/SurveyAnswers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SurveyAnswers]
+(
+	[AnswerID] VARCHAR(255) NOT NULL PRIMARY KEY, 
+    [UserID] VARCHAR(MAX) NOT NULL, 
+    [QuestionID] VARCHAR(255) NOT NULL, 
+    [SubmissionDate] DATETIME NOT NULL, 
+    CONSTRAINT [FK_SurveyAnswers_ToTable] FOREIGN KEY ([QuestionID]) REFERENCES [SurveyQuestions]([QuestionID])
+)
---
+++ SP-Y4C/Y4C-DB/SurveyQuestions.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SurveyQuestions]
+(
+	[QuestionId] VARCHAR(255) NOT NULL PRIMARY KEY, 
+    [Title] VARCHAR(50) NOT NULL, 
+    [Description] VARCHAR(MAX) NOT NULL, 
+    [Type] VARCHAR(50) NOT NULL, 
+    [ActiveStatus] TINYINT NOT NULL, 
+    [Weight] VARCHAR(50) NOT NULL, 
+    [CreationDate] DATETIME NOT NULL
+)
