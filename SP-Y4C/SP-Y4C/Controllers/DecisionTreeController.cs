93a8410da630921fc17914d4e6f532991727b554
James Webb <jwebb@intelligenttransactions.com>
2020-01-28 20:46:50 -0500

    Merging James's code into master

---
+++ SP-Y4C/SP-Y4C/Controllers/DecisionTreeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SP_Y4C.Controllers
+{
+    public class DecisionTreeController : Controller
+    {
+        public IActionResult FirstPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Teach()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Practice()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NotSure()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NextStep(IFormCollection form)
+        {
+            //Need it to follow the below coding logic
+            //Choose a starting page from three options
+            //teach, practice, and volunteering
+            //teach - are you interested ? teacher : donate
+            //practice - are you interested ? alumni : donate
+            //volunteering - are you interested ? volunteer : donate
+            switch (form["page"])
+            {
+                case "teach":
+                    if (form["teachInterest"].Equals("yes")){
+                        return Redirect("https://www.y4c.org/teacher-page");
+                    }else{
+                        return Redirect("https://www.y4c.org/donate");
+                    }
+                case "practice":
+                    if (form["gettingInvolved"].Equals("yes")){
+                        return Redirect("https://www.y4c.org/new-page-4");
+                    }else{
+                        return Redirect("https://www.y4c.org/donate");
+                    }
+                case "notSure": 
+                    if (form["gettingInvolved"].Equals("yes")){
+                        return Redirect("https://www.y4c.org/volunteer-1");
+                    }else{
+                        return Redirect("https://www.y4c.org/donate");
+                    }
+                default:
+                    return Redirect("https://www.y4c.org/donate");
+            }
+        }
+    }
+}
